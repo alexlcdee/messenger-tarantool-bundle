b5c7dd6ba74f01423ad46c26b539a10f1eaf0bb9
alexlcdee <info@alexlcdee.ru>
2020-05-07 23:40:16 +0300

    Init

---
+++ MessengerTarantoolBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlexLcDee\MessengerTarantoolBundle;
+
+
+use AlexLcDee\Messenger\Tarantool\Transport\QueueTransportFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MessengerTarantoolBundle extends Bundle
+{
+
+}
