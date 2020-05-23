4b117d2c3e786c72bb4538a5b7ff442bf1ee35c6
alexlcdee <info@alexlcdee.ru>
2020-05-23 11:01:48 +0300

    First release

---
+++ TarantoolClientFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlexLcDee\MessengerTarantoolBundle;
+
+
+class TarantoolClientFactory implements ClientFactory
+{
+    public function fromDsn(string $dsn)
+    {
+        if(class_exists(\Tarantool\Client\Client::class)) {
+            return (new PhpClientFactory())->fromDsn($dsn);
+        } elseif(class_exists(\Tarantool::class)) {
+            return (new ExtensionClientFactory())->fromDsn($dsn);
+        } else {
+            $message = <<<TXT
+No suitable implementations of Tarantool Client found!
+
+You must use php tarantool/client 
+```composer require tarantool/client```
+or tarantool pecl extension
+```
+pecl channel-discover tarantool.github.io/tarantool-php/pecl
+pecl install Tarantool-PHP/Tarantool-beta
+```
+TXT;
+
+            throw new \LogicException($message);
+        }
+    }
+}
