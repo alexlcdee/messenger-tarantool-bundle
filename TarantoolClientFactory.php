6f0dfb13926ffdc22e18792110c9b6f64fabf4b4
alexlcdee <info@alexlcdee.ru>
2020-05-23 11:32:28 +0300

    fix client factory

--- TarantoolClientFactory.php
+++ TarantoolClientFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AlexLcDee\MessengerTarantoolBundle;
 
+use AlexLcDee\Messenger\Tarantool\Tarantool\ClientFactory;
 
 class TarantoolClientFactory implements ClientFactory
 {
